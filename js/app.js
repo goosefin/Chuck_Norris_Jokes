2bd30463b033635d8500b2421ac60f1ed790ffc7
goosefin <agustinafin96@gmail.com>
2023-03-14 19:32:54 -0400

    Fetch call successfully retrieving jokes

--- js/app.js
+++ js/app.js
@@ -1,7 +1,21 @@
 let button = document.querySelector('#btn')
+let title = document.querySelector('h1')
 
+//fetch joke
 let joke = () =>{
-    fetch()
+    fetch('https://api.chucknorris.io/jokes/random')
+    //parse data
+    .then((response) => response.json())
+    .then((data) => {
+        //create new element
+        let joke = document.createElement('h2')
+        //append
+        document.body.appendChild(joke)
+        //inner text
+        joke.innerText = data.value
+    })
+    
+    
 }
 
 button.addEventListener('click', joke)
